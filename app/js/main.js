e91df017e849aa65235bec7f907e716a4cfa4a90
Artem <parov91@mail.ru>
2021-09-20 22:42:46 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  $('.slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/slider/icon-arrow-left.svg" alt="стрелка влево"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/slider/icon-arrow-right.svg" alt="стрелка вправо"></button>'
+  });
+
+  var mixer = mixitup('.top__content');
+})
