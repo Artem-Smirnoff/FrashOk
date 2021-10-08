174ab03566ef80cdc194ff610f73dfa65ad4e416
Artem <parov91@mail.ru>
2021-10-08 23:44:12 +0300

    added drinks

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,15 @@ $(function () {
     nextArrow: '<button type="button" class="slick-next"><img src="images/slider/icon-arrow-right.svg" alt="стрелка вправо"></button>'
   });
 
- 
-  var mixer = mixitup('.top-products__content');
-  var mixer = mixitup('.actions__list');
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 })
